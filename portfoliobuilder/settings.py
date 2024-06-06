9e3bf99ba2f4c84a60ee0217b3056ac9337250a2
faiyazAziz <grdfaiyaz786@gmail.com>
2024-06-06 18:50:37 +0530

    static_files error

--- portfoliobuilder/settings.py
+++ portfoliobuilder/settings.py
@@ -140,3 +140,14 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STORAGES = {
+            "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
