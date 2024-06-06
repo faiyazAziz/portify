7e614546493ec5df9c45b702ea0930850d464b46
faiyazAziz <grdfaiyaz786@gmail.com>
2024-06-06 20:32:45 +0530

    change for database

--- portfoliobuilder/settings.py
+++ portfoliobuilder/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['portify-1.onrender.com']
+ALLOWED_HOSTS = ['portify-1.onrender.com','http://127.0.0.1:8000']
 
 # Application definition
 
