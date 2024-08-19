2e8911daf09229f752dae03f4e9234a74cfc16e5
faiyazAziz <grdfaiyaz786@gmail.com>
2024-08-19 14:14:53 +0530

    update

--- portfoliobuilder/settings.py
+++ portfoliobuilder/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# ALLOWED_HOSTS = ['portify-in.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portify-in.onrender.com']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
